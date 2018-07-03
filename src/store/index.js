d2c8d6507d13e216cf75f345ff584896c2b48687
Gouthami M N <gouthami_mn@infosys.com>
2018-07-03 22:53:53 +0200

    fetching more pages from schiphol API

--- src/store/index.js
+++ src/store/index.js
@@ -5,7 +5,7 @@ import moment from "moment"
 
 Vue.use(Vuex)
 const baseURL =
-  "https://api.schiphol.nl/public-flights/flights?app_id=0e2669b9&app_key=e77dc4046db400cfa757f84d7c5f3650&flightdirection=A&includedelays=false&page=0&sort=%2Bscheduletime"
+  "https://api.schiphol.nl/public-flights/flights?app_id=0e2669b9&app_key=e77dc4046db400cfa757f84d7c5f3650&flightdirection=A&includedelays=false&sort=%2Bscheduletime"
 
 export const store = new Vuex.Store({
   state: {
@@ -48,12 +48,32 @@ export const store = new Vuex.Store({
   actions: {
     async callSchipholAPI({ commit }) {
       let currentTime = new Date((new Date()).getTime() - 1800000);
+      let response = []
+
+      // first call to API
       let url = `${baseURL}&scheduledate=${currentTime.getFullYear()}-${currentTime.getMonth()}-${currentTime.getDate()}&scheduletime=${currentTime.getHours()}%3A${currentTime.getMinutes()}`
-      let response = await axios.get(url, {
+      let result = await axios.get(url, {
         headers: { ResourceVersion: "v3" }
       })
+      response = response.concat(result.data.flights)
+
+      // Getting total number of pages from header - start
+      let link = ((result.headers.link.split(';')[0]).replace("<", "")).replace(">", "")
+      var regex = new RegExp("[?&]page(=([^&#]*)|&|#|$)"),
+      results = regex.exec(link)
+      let noOfPages = decodeURIComponent(results[2].replace(/\+/g, " "));
+      // Getting total number of pages from header - end
+
+      // getting more pages - max 10 or total number of pages(whichever is lesser)
+      for (let pageCount = 0; pageCount < 10 && pageCount <= noOfPages; pageCount++) {
+        let url = `${baseURL}&scheduledate=${currentTime.getFullYear()}-${currentTime.getMonth()}-${currentTime.getDate()}&scheduletime=${currentTime.getHours()}%3A${currentTime.getMinutes()}&page=${pageCount}`
+        result = await axios.get(url, {
+          headers: { ResourceVersion: "v3" }
+        })
+        response = response.concat(result.data.flights)
+      }
       commit("setUpdatedTime", moment().format("DD-MM-YYYY, HH:mm:ss"))
-      commit("setFlights", response.data.flights)
+      commit("setFlights", response)
     }
   }
 })
